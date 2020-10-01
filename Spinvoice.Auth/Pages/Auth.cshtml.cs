52730de33126a6cca957cb07c5e8db10fd6aff11
a.ukolov <aukolov@gmail.com>
2020-10-01 20:41:50 +0300

    first commit

---
+++ Spinvoice.Auth/Pages/Auth.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Spinvoice.Auth.Pages
+{
+    public class AuthModel : PageModel
+    {
+        private readonly ILogger<AuthModel> _logger;
+
+        public AuthModel(ILogger<AuthModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string AuthKey => $"{Request.Query["code"]}.{Request.Query["realmId"]}";
+
+        public void OnGet()
+        {
+        }
+    }
+}
